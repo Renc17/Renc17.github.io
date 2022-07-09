2e5e002894423f779599567ed7d48e5d51c7b713
Renc17 <cangaprogram@gmail.com>
2022-07-10 02:52:57 +0300

    fix: banner, style, add: clear

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,6 +1,5 @@
 help = [
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-2">whois</span><span>Who is Rena?</span>',
+  '<span class="neonlabel no-animation col-2 mt-2">whois</span><span class="mt-2">Who is Rena?</span>',
   '<span class="neonlabel no-animation col-2">projects</span><span>View coding projects</span>',
   '<span class="neonlabel no-animation col-2">skills</span><span>Things I\'m good at</span>',
   '<span class="neonlabel no-animation col-2">experience</span><span>Yeah, I have a little of this too</span>',
@@ -8,19 +7,16 @@ help = [
   '<span class="neonlabel no-animation col-2">social</span><span>Display social networks</span>',
   '<span class="neonlabel no-animation col-2">contact</span><span>Bring it on</span>',
   '<span class="neonlabel no-animation col-2">banner</span><span>Display the header</span>',
-  '<span class="neonlabel no-animation col-2">clear</span><span>Clear Terminal</span>',
-  "<div><br></div>",
+  '<span class="neonlabel no-animation col-2 mb-2">clear</span><span>Clear Terminal</span>',
 ];
 
 whois = [
-  "<div><br></div>",
   "Hi there, I'm Rena! &#128075;",
-  "I'm a backend engineer with strong problem solving skills. Motivated to advance and",
+  "I'm a software engineer with strong problem solving skills. Motivated to advance and",
   "expand my skill set through challenging projects and mentorship. In my opinion team",
   "work is the key to every successful project.",
   "Meanwhile on my free time I take photos and travel as much as I can. I'm a fan of ",
   "the outdoors and exploring but I also enjoy some peaceful and cozy time back home.",
-  "<div><br></div>",
 ];
 
 instagram = "https://www.instagram.com/tsagkaa/";
@@ -28,103 +24,86 @@ linkledin = "https://www.linkedin.com/in/rena-canga-723542244/";
 github = "https://www.github.com/renc17/";
 
 social = [
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-2">instagram</span><span><a class="base-color" href="' +
+  '<span class="neonlabel no-animation col-2 mt-2">instagram</span><span class="mt-2"><a class="base-color" href="' +
     instagram +
     '" target="_blank">instagram/tsagkaa</a></span>',
   '<span class="neonlabel no-animation col-2">linkedIn</span><span><a class="base-color" href="' +
     linkledin +
     '" target="_blank">linkledin/renacanga</a></span>',
-  '<span class="neonlabel no-animation col-2">github</span><span><a class="base-color" href="' +
+  '<span class="neonlabel no-animation col-2 mb-2">github</span><span><a class="base-color" href="' +
     github +
     '" target="_blank">github/Renc17</a></span>',
-  "<div><br></div>",
 ];
 
 projects = [
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-3 mb-2">Doatap | University | php</span>',
+  '<span class="neonlabel no-animation col-3 mt-2 mb-1">Doatap | University | php</span>',
   '     <a target="_blank" href="https://www.github.com/Renc17/Doatap">Source code</a>',
   '     • Redesign a government website (diploma recognition) for more usability and user-friendly interface. see  <a target="_blank" href="https://www.youtube.com/watch?v=lUGoOuhDU4s&list=PLXKopXHbhib9sAlftkMec8kKnKwxgpynv&index=10">youtube video</a>',
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-3 mb-2">Booking Website | University | java/typescript </span>',
+  '<span class="neonlabel no-animation col-3 mt-3 mb-1">Booking Website | University | java/typescript </span>',
   '     <a target="_blank" href="https://www.github.com/Renc17/BookingWebsite">Source code</a>',
   "     • Booking.com website for short term rentals",
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-3 mb-2">Canon API | Personal | javascript </span>',
+  '<span class="neonlabel no-animation col-3 mt-3 mb-1">Canon API | Personal | javascript </span>',
   '     <a target="_blank" href="https://www.github.com/Renc17/Canon-API">Source code</a>',
   "     • Fake store REST API that you can use whenever you need Pseudo-real data for your ecommerce website <br>     without running any server-side code. It's great for website prototype, testing, etc.",
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-3 mb-2">Project Automation | Personal | python </span>',
+  '<span class="neonlabel no-animation col-3 mt-3 mb-1">Project Automation | Personal | python </span>',
   '     <a target="_blank" href="https://www.github.com/Renc17/Project_Automation">Source code</a>',
   "     • Automate the process of creating and linking a new github repo and a local folder every time you begin a new project.",
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-3 mb-2">Bookksamil | Personal | typescript/javascript </span>',
+  '<span class="neonlabel no-animation col-3 mt-3 mb-1">Bookksamil | Personal | typescript/javascript </span>',
   '     <a target="_blank" href="https://www.github.com/Renc17/GlobalTravel">Source code</a>',
   '     <a target="_blank" href="http://bookksamil.com/">Website</a>',
   "     • Custom made website for a client's business. ",
   "     • Tour booking, accommodation, car rental, restaurant suggestions, etc",
-  "<div><br></div>",
+  "",
 ];
 
 skills = [
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-2">typescript</span><span>nestjs</span>',
+  '<span class="neonlabel no-animation col-2 mt-2">typescript</span><span class="mt-2">nestjs</span>',
   '<span class="neonlabel no-animation col-2">javascript</span><span>express, nodejs</span>',
   '<span class="neonlabel no-animation col-2">php</span><span>laravel</span>',
   '<span class="neonlabel no-animation col-2">sql</span><span>postgres, mysql</span>',
   '<span class="neonlabel no-animation col-2">git</span><span>github</span>',
-  '<span class="neonlabel no-animation col-2">linux</span><span>bash scripting</span>',
-  "<div><br></div>",
+  '<span class="neonlabel no-animation col-2 mb-2">linux</span><span>bash scripting</span>',
 ];
 
 education = [
-  "<div><br></div>",
   '<span class="neonlabel no-animation mt-2 mb-2">Computer Science</span><span class="ms-1 mt-2"> &#129299; </span> ',
   "National And Kapodistrian University of Athens | 2017",
   "   Specialty: ",
   "   • Data and Knowledge Management",
   "   • Software",
-  "<div><br></div>",
 ];
 
 experiance = [
-  "<div><br></div>",
   '<span class="neonlabel no-animation mt-2 mb-2">Backend Engineer Intern</span><span class="ms-1 mt-2"> &#128585; </span>',
-  "Quintessential SFT  |  September 2021 - March 2022",
+  "Quintessential SFT | September 2021 - March 2022",
   "   • Created a Logging Library",
   "   • Partnered with the backend engineer team to maintain and optimize Waiz app",
   "   • Partnered with the company's Data Analyst and created validation scripts for data files from AB Basilopoulos",
-  "<div><br></div>",
 ];
 
 contact = [
-  "<div><br></div>",
-  '<span class="neonlabel no-animation col-2">email</span><a href="mailto:rtsagka@gmail.com" class="text-decoration-none base-color">rtsagka@gmail.com</a>.',
-  "<div><br></div>",
+  '<span class="neonlabel no-animation col-2 mt-2 mb-2">email</span><a href="mailto:rtsagka@gmail.com" class="text-decoration-none base-color">rtsagka@gmail.com</a>.',
 ];
 
 banner = [
-  "<br>",
-  "            ***     ***",
-  "            ***     ***",
+  "            ***     ***            ",
+  "            ***     ***            ",
   "**       *****************       **",
   "**      *********************    **",
   "**    ************************   **",
   "**    ************************   **",
   "**    ******   *****   *******   **",
-  "************   *****   ************      **********    ",
-  "  **********   *****   **********        **********    ",
-  "      ************************               ***       ",
-  "      ************************               ***  *****  ******  ****** **  * ******   ",
-  "      ***********************                ***  **     **  **  **  ** ** *  **  **   ",
-  "         ******     ******                   ***     **  **  **  **  ** ** *  **  **   ",
-  "         ******     ******                   ***  *****  ******* ****** **  * *******  ",
-  "      *****            *****                                      **  ",
-  "      *****            *****                                   ******  ",
-
-  "<br>",
+  "************   *****   ************",
+  "  **********   *****   **********  ",
+  "      ************************     ",
+  "      ************************     ",
+  "      ***********************          ____                      ______                 __        ",
+  "         ******     ******            / __ \\___  ____  ____ _   /_  __/________ _____ _/ /______ _",
+  "         ******     ******           / /_/ / _ \\/ __ \\/ __ `/    / / / ___/ __ `/ __ `/ //_/ __ `/",
+  "      *****            *****        / _, _/  __/ / / / /_/ /    / / (__  ) /_/ / /_/ / ,< / /_/ / ",
+  "      *****            *****       /_/ |_|\\___/_/ /_/\\__,_/    /_/ /____/\\__,_/\\__, /_/|_|\\__,_/  ",
+  "_____,;;;;;;;;;;;;;;;;;;;;;;;;,.___ __ __________                             /____/                                                                           ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~",
   "<span>&#128640; Welcome to my interactive web terminal</span>",
-  "<span>For a list of available commands, type <span class=\"neonlabel\"> ' help ' </span> .</span>",
-  "<br>",
+  "<span>For a list of available commands, type <span class=\"neonlabel\">'help'</span> .</span>",
 ];
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var before = document.getElementById("before");
+var before = "";
 var command = document.getElementById("typer");
 var textarea = document.getElementById("texter");
 
@@ -7,8 +7,10 @@ var commands = [];
 
 // intialize the terminal
 setTimeout(function () {
-  newprompt("");
-  loopLines(banner, "colored", 80);
+  addlog();
+  before = document.getElementById("before");
+  newprompt("", "");
+  loopLines(banner, "ps-5 colored", 80);
   textarea.focus();
 }, 100);
 
@@ -18,14 +20,14 @@ window.onload = function () {
 
 window.addEventListener("keyup", enterKey);
 
-function newprompt(text) {
+function newprompt(text, style) {
   var prompt = document.createElement("span");
   prompt.innerHTML = "~";
 
   var div = document.createElement("div");
   var arrow = document.createElement("span");
   arrow.innerHTML = ">";
-  arrow.className = "arrow";
+  arrow.className = "arrow " + style;
   div.appendChild(arrow);
 
   var cmd = document.createElement("span");
@@ -40,40 +42,52 @@ function commander(cmd) {
   cmd = cmd.replace(new RegExp(/\r?\n|\r/g), "");
   switch (cmd.toLowerCase()) {
     case "whois":
+      newprompt(textarea.value, "");
       loopLines(whois, "ps-5 ", 80);
       break;
     case "help":
+      newprompt(textarea.value, "");
       loopLines(help, "ps-5 d-flex", 80);
       break;
     case "projects":
+      newprompt(textarea.value, "");
       loopLines(projects, "ps-5 d-flex", 80);
       break;
     case "skills":
+      newprompt(textarea.value, "");
       loopLines(skills, "ps-5 d-flex", 80);
       break;
     case "experience":
+      newprompt(textarea.value, "");
       loopLines(experiance, "ps-5 d-flex", 80);
       break;
     case "education":
+      newprompt(textarea.value, "");
       loopLines(education, "ps-5 d-flex", 80);
       break;
     case "social":
+      newprompt(textarea.value, "");
       loopLines(social, "ps-5 d-flex", 80);
       break;
     case "contact":
+      newprompt(textarea.value, "");
       loopLines(contact, "ps-5 d-flex", 80);
       break;
     case "banner":
-      loopLines(banner, "colored", 80);
+      newprompt(textarea.value, "");
+      loopLines(banner, "ps-5 colored", 80);
       break;
     case "":
+      newprompt(textarea.value, "");
       break;
     case "clear":
+      clear();
       break;
     default:
+      newprompt(textarea.value, "error-arrow");
       addLine(
         '<span class="colored">' + cmd + ": Command not found.</span>",
-        "error mb-3",
+        "mb-3",
         100
       );
       break;
@@ -88,7 +102,7 @@ function typeIt(from, e) {
 
 function enterKey(e) {
   if (e.keyCode == 13) {
-    newprompt(textarea.value);
+    console.log("dfkh");
     commander(textarea.value);
 
     textarea.value = "";
@@ -97,17 +111,9 @@ function enterKey(e) {
 }
 
 function addLine(text, style, time) {
-  var t = "";
-  for (let i = 0; i < text.length; i++) {
-    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
-      t += "&nbsp;&nbsp;";
-    } else {
-      t += text.charAt(i);
-    }
-  }
   setTimeout(function () {
-    var next = document.createElement("p");
-    next.innerHTML = t;
+    var next = document.createElement("div");
+    next.innerHTML = text;
     next.className = style;
 
     before.parentNode.insertBefore(next, before);
@@ -120,4 +126,27 @@ function loopLines(name, style, time) {
   name.forEach(function (item, index) {
     addLine(item, style, index * time);
   });
+}
+
+function clear() {
+  var logs = document.getElementById("log");
+  logs.remove();
+
+  addlog();
+
+  before = document.getElementById("before");
+  command = document.getElementById("typer");
+  textarea = document.getElementById("texter");
+}
+
+function addlog() {
+  var div = document.createElement("div");
+  div.id = "log";
+  div.className = "ascii-art";
+  var a = document.createElement("a");
+  a.id = "before";
+  div.appendChild(a);
+
+  var prompt = document.getElementById("prompt");
+  prompt.parentNode.insertBefore(div, prompt);
 }
